f17f281de3dd83227b35bcd9d900776638430d22
Eric Dimech <ericdimech@hotmail.com>
2023-12-04 13:15:44 -0500

    Add homecomponent test data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { Housinglocation } from "../housinglocation";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   template: `
@@ -17,6 +18,19 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
       <app-housing-location></app-housing-location>
     </section>
   `,
-  styleUrl: './home.component.css',
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = "https://angular.io/assets/images/tutorials/faa";
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: "Test Home",
+    city: "Test city",
+    state: "ST",
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
